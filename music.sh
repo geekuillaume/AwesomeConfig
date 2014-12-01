2b8ab44ccf521dba4f0113329ead17997e582612
Geekuillaume <geek.uillaume@gmail.com>
2014-12-01 18:54:05 +0100

    Improved the script to get spotify current playing song

--- music.sh
+++ music.sh
@@ -1,2 +1,26 @@
 #! /bin/sh
-dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | tr '\n' ' ' | sed 's/.*title"\s*variant\s*string "\([^\"]*\)".*/\1/' && echo -n " - " && dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | tr '\n' ' ' | sed 's/.*artist"\s*variant\s*array \[\s*string "\([^\"]*\)".*/\1/' && echo ""
+
+infos=$(dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata')
+
+
+title=$(echo "$infos" \
+| tr '\n' ' ' \
+| sed 's/.*title"\s*variant\s*string "\([^\"]*\)".*/\1/')
+
+artist=$(echo "$infos" \
+| tr '\n' ' ' \
+| sed 's/.*artist"\s*variant\s*array \[\s*string "\([^\"]*\)".*/\1/')
+
+lenTitle=$(echo ${#title})
+lenArtist=$(echo ${#artist})
+
+if [ $lenTitle -gt 30 ]
+  then
+    title=$(echo "$title" | cut -c-30 | sed 's/ *$//' | tr '\n' '.' && echo -n "..")
+fi
+if [ $lenArtist -gt 30 ]
+  then
+    artist=$(echo "$artist" | cut -c-30 | sed 's/ *$//' | tr '\n' '.' && echo -n "..")
+fi
+echo "$title - $artist"
+
